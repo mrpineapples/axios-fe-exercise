fae7a9f3e17c2e60995e1b0d4afe741b3c39a4d1
mrpineapples <michaelmiranda161@gmail.com>
2021-09-03 12:03:48 -0400

    feat: SSR results

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
 import { useWindowSize } from "../hooks/useWindowSize";
 import { Carousel } from "../components/Carousel";
@@ -7,33 +6,18 @@ import { Header } from "../components/Header";
 import { Story } from "../components/Story";
 import { GlobalStyles } from "../styles";
 
-const Index = () => {
-    const [stories, setStories] = useState(null);
+const Index = ({ storiesData }) => {
     const { isMobile } = useWindowSize();
-
-    useEffect(() => {
-        (async () => {
-            const { data: streamData } = await axios.get(
-                "https://api.axios.com/api/render/stream/content/"
-            );
-            const { results } = streamData;
-            const storiesToGet = results.map((id) =>
-                axios.get(`https://api.axios.com/api/render/content/${id}`)
-            );
-            const storiesData = await Promise.all(storiesToGet);
-            setStories(storiesData);
-        })();
-    }, []);
 
     return (
         <>
             <GlobalStyles />
             <Header />
-            {stories && (
+            {storiesData && (
                 <>
                     <Carousel>
-                        {stories?.map(({ data }) => {
-                            const {
+                        {storiesData?.map(
+                            ({
                                 id,
                                 headline,
                                 topics,
@@ -41,22 +25,23 @@ const Index = () => {
                                 permalink,
                                 primary_image,
                                 social_image, // fallback in case of no primary
-                            } = data;
-                            const { alt_text, base_image_url } =
-                                primary_image || social_image || {};
+                            }) => {
+                                const { alt_text, base_image_url } =
+                                    primary_image || social_image || {};
 
-                            return (
-                                <Story
-                                    key={id}
-                                    altText={alt_text}
-                                    headline={headline}
-                                    topic={topics?.[0]?.name}
-                                    permaLink={permalink}
-                                    publishedDate={published_date}
-                                    imageURL={base_image_url}
-                                />
-                            );
-                        })}
+                                return (
+                                    <Story
+                                        key={id}
+                                        altText={alt_text}
+                                        headline={headline}
+                                        topic={topics?.[0]?.name}
+                                        permaLink={permalink}
+                                        publishedDate={published_date}
+                                        imageURL={base_image_url}
+                                    />
+                                );
+                            }
+                        )}
                     </Carousel>
                     {isMobile && <HomeButton />}
                 </>
@@ -64,5 +49,29 @@ const Index = () => {
         </>
     );
 };
+
+export async function getServerSideProps(context) {
+    const { data: streamData } = await axios.get(
+        "https://api.axios.com/api/render/stream/content/"
+    );
+    const { results } = streamData;
+    const storiesToGet = results.map((id) =>
+        axios.get(`https://api.axios.com/api/render/content/${id}`)
+    );
+    const rawStoriesData = await Promise.all(storiesToGet);
+    const storiesData = rawStoriesData.map((result) => result.data);
+
+    if (!streamData && !storiesData) {
+        return {
+            notFound: true,
+        };
+    }
+
+    return {
+        props: {
+            storiesData,
+        },
+    };
+}
 
 export default Index;
